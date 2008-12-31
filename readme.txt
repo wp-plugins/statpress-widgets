de65b262922bf73f818c63209c30247a8467fe87
hoodajan <hoodajan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-31 02:05:26 +0000

    '0.18'
    git-svn-id: https://plugins.svn.wordpress.org/statpress-widgets/trunk@83233 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== Plugin Name ===
+Contributors: jan
+Donate link: 
+Tags: statpress, statistics widget, widgets, referrers 
+Requires at least: 
+Tested up to: 2.7
+Stable tag: 0.18
+
+Create widgets by using the <a href="http://wordpress.org/extend/plugins/statpress-reloaded/">Statpress Plugin</a> data.
+
+== Description ==
+
+This first version generates sidebar for the the top and last referrers. 
+Features: In-/Excluding search engines, Bad words list,  display counts. 
+The widget is completle setable. 
+<br/>
+Next versions will contain widgets for top search engines and terms, top browsers, spiders an so on.	
+
+== Installation ==
+
+1. Extract the folder "sp-widgets" from the zip file and upload it to the "/wp-content/plugins/" directory
+2. Activate the plugin through the 'Plugins' menu in the WordPress admin interface
+3. Set up you're widget control settings.
+
+== Screenshots ==
+
+1. The referrers widget
+2. The widget controls
+
+
+
---
+++ sp-widgets.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Plugin Name: SP Widgets
+Plugin URI: http://blog.hooda.de/
+Description: Show widgets by using the <a href="http://wordpress.org/extend/plugins/statpress-reloaded/" target="_blank">Statpress Plugin</a> table data
+Version: 0.18
+Author: Jan A. Manolov
+Author URI: http://blog.hooda.de/
+*/
+
+
+	include "sp-widget-referrers.php";
+	
+
+	class SP_Widgets {
+		
+		static function init() {
+
+		  if (!function_exists('register_sidebar_widget')
+			|| !function_exists('register_widget_control'))
+				return;
+			
+			$widget_referrers = new SP_Widget_Referrers();
+		}
+		
+							
+		static function aktivate_plugin() {
+		
+			$options = array (
+				referrers_top_title 		=> 'Hot Referrers',
+				referrers_top_max 		=> 5,
+				referrers_top_counts 	=> 1,
+				referrers_last_title 	=> 'Last Referrers',
+				referrers_last_max 		=> 5,
+				referrers_badwords 		=> array('boln.cn'),
+				referrers_searchengines => 0
+			);
+			
+			add_option('sp_widgets_options', $options);
+		}
+		
+		static function deaktivate_plugin() {
+			delete_option('sp_widgets_options');
+		}		
+			
+	}
+		
+	add_action ('activate_'.plugin_basename(__FILE__), array('sp_widgets','aktivate_plugin'));
+	add_action ('deactivate_'.plugin_basename(__FILE__), array('sp_widgets','deaktivate_plugin'));
+	add_action ('plugins_loaded', array('sp_widgets','init'));
+	
+?>
